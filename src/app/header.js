6df4a604f7cc17306e8ad3801203a7e2ed57401f
misanthropicinc <netdecay2008@gmail.com>
2026-04-11 21:23:19 +0500

    poopy

---
+++ src/app/header.js
@@ -0,0 +1,12 @@
+import './header.css'
+
+export default function Header(){
+    return(
+        <>
+        <header>
+            <nav>
+            </nav>
+        </header>
+        </>
+    )
+}
