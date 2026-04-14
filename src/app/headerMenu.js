a0cbe17b90c963228528bdfed25fa40121851f1d
misanthropicinc <netdecay2008@gmail.com>
2026-04-14 11:32:28 +0500

    menu content

--- src/app/headerMenu.js
+++ src/app/headerMenu.js
@@ -5,10 +5,25 @@ export default function HeaderMenu({ isOpen, isSticky, onClose }) {
   return (
     <>
       {isOpen && (
-        <div className={`menu-backdrop ${isSticky ? "sticky" : ""}`} onClick={onClose} />
+        <div
+          className={`menu-backdrop ${isSticky ? "sticky" : ""}`}
+          onClick={onClose}
+        />
       )}
-      <div className={`header-menu ${isOpen ? "open" : ""} ${isSticky ? "down" : "up"}`}>
-        <div className="menu-content"></div>
+      <div
+        className={`header-menu ${isOpen ? "open" : ""} ${isSticky ? "down" : "up"}`}
+      >
+        <div className="menu-content-wrapper">
+          <div className="menu-content">
+            <a className="menu-title">SHOP CATEGORY TITLE</a>
+            <div className="menu-categories">
+              <HeaderMenuCategory />
+              <HeaderMenuCategory />
+              <HeaderMenuCategory />
+              <HeaderMenuCategory />
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
--- src/app/headerMenuCategories.js
+++ src/app/headerMenuCategories.js
@@ -1,12 +1,16 @@
+import "./headerMenuCategories.css";
+
 export default function HeaderMenuCategory() {
   return (
     <div className="menu-category">
       <a className="menu-category-title">categoryT</a>
-      <a className="menu-category-btn">CATEGORY</a>
-      <a className="menu-category-btn">CATEGORY</a>
-      <a className="menu-category-btn">CATEGORY</a>
-      <a className="menu-category-btn">CATEGORY</a>
-      <a className="menu-category-btn">CATEGORY</a>
+      <div className="cat-btns">
+        <a className="menu-category-btn">CATEGORY</a>
+        <a className="menu-category-btn">CATEGORY</a>
+        <a className="menu-category-btn">CATEGORY</a>
+        <a className="menu-category-btn">CATEGORY</a>
+        <a className="menu-category-btn">CATEGORY</a>
+      </div>
     </div>
   );
 }
